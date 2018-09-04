3170a2cccad42afe81b3888b5e025c4d1fe37406
Cristian Marquez <xthecapx@gmail.com>
2018-09-03 19:58:24 -0500

    Added comments to the service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 
+// constructor(private service: AppService) {}
 @Injectable()
 export class AppService {
     private datos;
     private observer = new Subject();
     public $obs = this.observer.asObservable();
+    // this.service.$obs.subscribe((value) => { // Do something with value });
 
     inform(value) {
+        const newValue = Number(value);
         this.observer.next(value);
     }
+    // this.service.inform("new value");
 
     getDatos() {
         return this.datos;
